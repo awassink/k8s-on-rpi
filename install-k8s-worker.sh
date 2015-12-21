1cce391ef82253f947938540f0935b7f720ed8c5
Arjen Wassink <awassink@quintor.nl>
2015-12-21 02:01:55 +0100

    Added console output to the install script.

--- install-k8s-worker.sh
+++ install-k8s-worker.sh
@@ -4,6 +4,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 apt-get install bridge-utils
 
+echo "Copying kubernetes service configuration files"
 cp -f ./rootfs/lib/systemd/system/docker-bootstrap.service /lib/systemd/system/docker-bootstrap.service
 cp -f ./rootfs/lib/systemd/system/docker-bootstrap.socket /lib/systemd/system/docker-bootstrap.socket
 cp -f ./rootfs/lib/systemd/system/k8s-flannel.service /lib/systemd/system/k8s-flannel.service
@@ -11,14 +12,21 @@ cp -f ./rootfs/lib/systemd/system/docker.service /lib/systemd/system/docker.serv
 cp -f ./rootfs/lib/systemd/system/docker.socket /lib/systemd/system/docker.socket
 cp -f ./rootfs/lib/systemd/system/k8s-worker.service /lib/systemd/system/k8s-worker.service
 
+echo "Reloading the system service configuration"
 systemctl daemon-reload
 
+echo "Stopping the docker service"
 systemctl stop docker.service
 
+echo "Enabling the new services"
 systemctl enable docker-bootstrap.service k8s-flannel.service k8s-worker.service
 
+echo "Starting the docker bootstrap service"
 systemctl start docker-bootstrap.service
+echo "Starting the flannel service"
 systemctl start k8s-flannel.service
+echo "Starting the docker service"
 systemctl start docker.service
+echo "Starting the kubernetes worker service"
 systemctl start k8s-worker.service
 
